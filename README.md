4cb3357eac761eb74d799ee3a1f1966a42eec47a
RashidaBengali <bengali.rashida@gmail.com>
2019-05-01 08:49:28 +1000

    started with Readme

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
-# READ ME
-# Coming Soon
+# Project 1
+
+Restaurants on Rails
+--------------------------------------
+![Food](app/assets/images/goodfood.jpg)
+
+*Restaurants on Rails* is the new age Restaurant Finder. It helps you find the answer to the most difficult question: ***Where and What should I eat?***
+
+Most of you must have figured out the mystery behind the name of this Application. It's obvious because it has been built majorly using ```Rails``` and hence ***Users*** can look for their loved *Restaurant on Rails*.
+
+Any one who knows how to browse on the internet can use our service to find their favourite food place. Just search for the ***Cuisine*** you are craving for and we present you with a list of ***Restaurants*** that offers that particular Cuisine.
+
+[Click here to find out your favourite food place!](https://rashidabengali.github.io/tic-tac-toe/)
+
+---------------------------------------
+## Membership Types
+
+There are two types of Membership *Standard* & *Platinum*.
+
+![Membership](images/TicTacToe_ani.gif)
+
+#### Standard Membership
+*Standard* Users can create an account with us for absolutely **FREE** and they get the facility to *Comment*, ***Like*** and ***Review*** the Restaurants on our Page that will help them as well as other people make better dine in choices.
+
+![Account](images/TicTacToe_ani.gif)
+
+![Review](images/TicTacToe_ani.gif)
+
+**Note**: Any one can see the list of Restaurants and its Reviews posted by others. However, only Members can Like or add a Review on a particular Restaurant's page.
+
+![Login](images/TicTacToe_ani.gif)
+
+#### Platinum Membership
+*Platinum* Users can create an account with us for a mere amount of $20 and they get all the facilities of a Standard Member as well as are entitled to a BOOM BOOM 20% ***Discount*** on all the Restaurants listed on our page.
+
+---------------------------------------
+## Membership Rules
+
+You really need to be ***Hungry*** to either ***Sign Up*** or ***Sign In***
+
+![Login](images/TicTacToe_ani.gif)
+
+----------------------------------------
+Let's Begin
+________________________________________
+
+Cuisines
+------------------------------------------
+
+Simply search for your special **Cuisine** and satisfy your craving.
+
+![Cuisine](images/TicTacToe_ani.gif)
+
+![Cuisine](images/TicTacToe_ani.gif)
+
+Each Cuisine will present you with a list of **Restaurants** that specialize in it.
+
+![Restaurant](images/TicTacToe_ani.gif)
+
+Restaurants
+------------------------------------------
+You can find the following information on the ***Restaurant*** page that will help you make the best choice:
+
+- Treat to the Eye: Food ***Photos*** that will give you a visual demonstration of how the dishes will look like when served
+- ***Cuisine*** Type
+- ***Location*** of the Restaurant
+- Signature Dish
+- ***Contact*** number to book a table in advance
+- Average ***Rating*** and Number of ***Likes*** that will show the popularity of the Restaurant
+- Chance to go through all the ***Comments*** posted by people who have already tried the restaurant out
+
+![Restaurant](images/TicTacToe_ani.gif)
+
+#### Alternatively
+----------------------------------------------
+You can also search for the *Restaurants* directly from our list. The list shows the basic data like location and average rating of each Restaurant and in order to know more details for each, *Users* needs to click on it.
+
+![Restaurant](images/TicTacToe_ani.gif)
+
+The Restaurants are listed in the order of the number of ***Likes***. The one with highest number of likes will always render at the top of the page so that the ***Users*** can make choices based on the popularity if they prefer.
+
+![Restaurant](images/TicTacToe_ani.gif)
+
+
+Technical Terms
+-----------------------------------------
+Now, let's have a real conversation!
+
+***Restaurants on Rails*** has the following MVC (Model View Controller) Structure:
+
+- Cuisines
+- Restaurants
+- Reviews
+- Likes
+- Users
+
+This application has individual ```.erb files``` **HTML** pages for each **Model's** *Index*, *Show*, *Edit* and *New* **Views**. The methods are well defined in the **Controller** files creating an ```instance variable``` for each method in order for it to have access in the View Page and be able to render data on the screen.
+
+There is a vigorous use of **CSS** with a blend of attractive colours and plenty of hover effects. Each page has its own customized styling.
+
+There is a minimal amount of **Javascript** applied to enhance some CSS features.
+
+Major Features:
+----------------------------------------
+
+`chooseLevel()`
+- Player can choose the *Game Level* and the *Type of Rounds* the player is interested in
+
+`playGame()`
+- Gets executed when **3x3** board boxes are clicked for either *Single Round* or *Tournament* and the results are rendered on the screen
+
+`playGameHard()`
+- Gets executed when **4x4** board boxes are clicked for either *Single Round* or *Tournament* and the results are rendered on the screen
+
+`resetGame()`
+- Player can hit the reset button to start a *New Game*
+
+Acknowledgement
+-------------------------------------------
+- My *husband* who gave me hints to help me figure out the game logic
+- ***Special thanks*** to *Yianni* & *Joel* for helping out with instant solutions every time I was stuck with a problem
+
+Coming Soon
+---------------------------------------------
+* Player will be able to choose if he wants to play with the computer or with a friend online
+* Player will get the facility to choose from a range of fancy icons
+
+
+***Now, scroll up and click on the link and do the following:***
+
+```
+const enjoyGame = function () {
+  alert('Fun Unlimited!');
+};
+
+enjoyGame();
+```
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,43 +21,43 @@ u10 = User.create(:name => 'Nazhad Bari', :image => '/assets/naz.jpg', :email =>
 
 
 Cuisine.destroy_all
-c1 = Cuisine.create :name => 'Modern Australian'
-c2 = Cuisine.create :name => 'Malaysian'
-c3 = Cuisine.create :name => 'Italian'
-c4 = Cuisine.create :name => 'Dessert'
-c5 = Cuisine.create :name => 'Mexican'
-c6 = Cuisine.create :name => 'Lebanese'
-c7 = Cuisine.create :name => 'Chinese'
+c1 = Cuisine.create(:name => 'Modern Australian', :image => '/assets/Lamb.jpg')
+c2 = Cuisine.create(:name => 'Malaysian', :image => '/assets/malaysian.jpg')
+c3 = Cuisine.create(:name => 'Italian', :image => '/assets/italian.jpg')
+c4 = Cuisine.create(:name => 'Dessert', :image => '/assets/dessert.jpg')
+c5 = Cuisine.create(:name => 'Mexican', :image => '/assets/mexican.jpg')
+c6 = Cuisine.create(:name => 'Lebanese', :image => '/assets/lebanese.jpg')
+c7 = Cuisine.create(:name => 'Chinese', :image => '/assets/chinese.jpeg')
 
 Restaurant.destroy_all
-r1 = Restaurant.create(:name => '12-Micron', :location => 'Wynyard', :phone => 83222075, :specialities => 'Steaks', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r2 = Restaurant.create(:name => '@iconic', :location => 'Wynyard', :phone => 97093499, :specialities => 'Chicken Funghi', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
-r3 = Restaurant.create(:name => 'Volcano Steak House', :location => 'Parramatta', :phone => 87644966, :specialities => 'Ribs and Steaks', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r1 = Restaurant.create(:name => '12-Micron', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Wynyard', :phone => 83222075, :specialities => 'Steaks', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r2 = Restaurant.create(:name => '@iconic', :images => ['/assets/iconic1.jpg', '/assets/iconic2.jpg', '/assets/iconic3.jpg'], :location => 'Wynyard', :phone => 97093499, :specialities => 'Chicken Funghi', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
+r3 = Restaurant.create(:name => 'Volcano Steak House', :images => ['/assets/volcano1.jpg', '/assets/volcano2.jpg', '/assets/volcano3.jpg'], :location => 'Parramatta', :phone => 87644966, :specialities => 'Ribs and Steaks', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r4 = Restaurant.create(:name => 'Pappa Rich', :location => 'Bankstown', :phone => 87644966, :specialities => 'Beef curry', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
-r5 = Restaurant.create(:name => 'Ipoh Town', :location => 'Bankstown', :phone => 87644966, :specialities => 'Laksa noodles', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r6 = Restaurant.create(:name => 'Albee''s Kitchen', :location => 'Campsie', :phone => 87644966, :specialities => 'Poached chicken rice', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r4 = Restaurant.create(:name => 'Pappa Rich', :images => ['/assets/papa1.jpg', '/assets/papa2.jpg', '/assets/papa3.jpg'], :location => 'Bankstown', :phone => 87644966, :specialities => 'Beef curry', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
+r5 = Restaurant.create(:name => 'Ipoh Town', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Bankstown', :phone => 87644966, :specialities => 'Laksa noodles', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r6 = Restaurant.create(:name => 'Albee''s Kitchen', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Campsie', :phone => 87644966, :specialities => 'Poached chicken rice', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r7 = Restaurant.create(:name => 'Glacage', :location => 'Bankstown', :phone => 87644966, :specialities => 'Shakshouka', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
-r8 = Restaurant.create(:name => 'CASA', :location => 'Barangaroo', :phone => 87644966, :specialities => 'Wood Fire Pizzas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r9 = Restaurant.create(:name => 'Pizzeria Belluci', :location => 'Barangaroo', :phone => 87644966, :specialities => 'Thin Crust Pizzas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r7 = Restaurant.create(:name => 'Glacage', :images => ['/assets/glacage1.jpg', '/assets/glacage2.jpg', '/assets/glacage3.jpg'], :location => 'Bankstown', :phone => 87644966, :specialities => 'Shakshouka', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
+r8 = Restaurant.create(:name => 'CASA', :images => ['/assets/casa1.jpg', '/assets/casa2.jpg', '/assets/casa3.jpg'], :location => 'Barangaroo', :phone => 87644966, :specialities => 'Wood Fire Pizzas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r9 = Restaurant.create(:name => 'Pizzeria Belluci', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Barangaroo', :phone => 87644966, :specialities => 'Thin Crust Pizzas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r10 = Restaurant.create(:name => 'Koi Dessert Bar', :location => 'Surry Hills', :phone => 87644966, :specialities => 'Strawberry pillow', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
-r11 = Restaurant.create(:name => 'Black Star Pastry', :phone => 87644966, :location => 'Newtown', :specialities => 'Watermelon pastry', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
-r12 = Restaurant.create(:name => 'Bay Vista', :location => 'Parramatta', :phone => 87644966, :specialities => 'Ice cream buckets', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r13 = Restaurant.create(:name => 'San Churros', :location => 'Penrith', :phone => 87644966, :specialities => 'Churros', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r10 = Restaurant.create(:name => 'Koi Dessert Bar', :images => ['https://cdn.concreteplayground.com/content/uploads/2018/01/KOIXMC3_Credit_Tery_Gunata.jpg', '/assets/koi2.jpg', '/assets/koi3.jpg'], :location => 'Surry Hills', :phone => 87644966, :specialities => 'Strawberry pillow', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
+r11 = Restaurant.create(:name => 'Black Star Pastry', :phone => 87644966, :images => ['/assets/black1.jpg', '/assets/black2.jpg', 'https://static1.squarespace.com/static/528357bae4b04e03f11d4913/t/55d2b19be4b0ac7807b89870/1439871389718/Black+Star+Pasty+Orange+Cake+w.+Persian+Fig+and+Cream+Cheese+Icing+x+Tippity+Tea'], :location => 'Newtown', :specialities => 'Watermelon pastry', :opening_hours => '9:00:00', :closing_hours => '20:00:00')
+r12 = Restaurant.create(:name => 'Bay Vista', :images => ['/assets/bay1.jpg', '/assets/bay2.jpg', '/assets/bay3.jpg'], :location => 'Parramatta', :phone => 87644966, :specialities => 'Ice cream buckets', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r13 = Restaurant.create(:name => 'San Churros', :images => ['/assets/san1.jpg', '/assets/san2.jpeg', '/assets/san3.jpg'], :location => 'Penrith', :phone => 87644966, :specialities => 'Churros', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r14 = Restaurant.create(:name => 'El Loco', :location => 'Townhall', :phone => 87644966, :specialities => 'Tacos', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r15 = Restaurant.create(:name => 'Mejico', :location => 'Surry Hills', :phone => 87644966, :specialities => 'Quesadillas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r16 = Restaurant.create(:name => 'Chica Bonita', :location => 'Manly', :phone => 87644966, :specialities => 'Burritos', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r14 = Restaurant.create(:name => 'El Loco', :images => ['/assets/loco1.jpeg', '/assets/loco2.jpg', '/assets/loco3.jpg'], :location => 'Townhall', :phone => 87644966, :specialities => 'Tacos', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r15 = Restaurant.create(:name => 'Mejico', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Surry Hills', :phone => 87644966, :specialities => 'Quesadillas', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r16 = Restaurant.create(:name => 'Chica Bonita', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Manly', :phone => 87644966, :specialities => 'Burritos', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r17 = Restaurant.create(:name => 'Jasmin 1', :location => 'Bankstown', :phone => 87644966, :specialities => 'Mixed grill platter', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
-r18 = Restaurant.create(:name => 'Anason', :location => 'Barangaroo', :phone => 87644966, :specialities => 'Hummus, Pita', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r17 = Restaurant.create(:name => 'Jasmin 1', :images => ['/assets/jasmin1.jpg', '/assets/jasmin2.jpg', '/assets/jasmin3.jpg'], :location => 'Bankstown', :phone => 87644966, :specialities => 'Mixed grill platter', :opening_hours => '9:00:00', :closing_hours => '22:00:00')
+r18 = Restaurant.create(:name => 'Anason', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Barangaroo', :phone => 87644966, :specialities => 'Hummus, Pita', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r19 = Restaurant.create(:name => 'Chef''s Dynasty', :location => 'Bankstown', :phone => 87644966, :specialities => 'Yum Cha Buffet', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
-r20 = Restaurant.create(:name => 'Twisted Noodles', :location => 'Punchbowl', :phone => 87644966, :specialities => 'Noodles', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r19 = Restaurant.create(:name => 'Chef''s Dynasty', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Bankstown', :phone => 87644966, :specialities => 'Yum Cha Buffet', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r20 = Restaurant.create(:name => 'Twisted Noodles', :images => ['/assets/micron1.jpeg', '/assets/micron2.jpg', '/assets/micron3.jpeg'], :location => 'Punchbowl', :phone => 87644966, :specialities => 'Noodles', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
-r21 = Restaurant.create(:name => 'Rashays', :location => 'Punchbowl', :phone => 87644966, :specialities => 'Beef Wagyu', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
+r21 = Restaurant.create(:name => 'Rashays', :images => ['/assets/rashay1.jpg', 'https://res.cloudinary.com/scentre-group-au/image/fetch/c_fill,q_auto,g_faces:auto,w_2500,h_1071,f_auto/https://cam.scentregroup.io/m/3e9560583e139b26', '/assets/rashay3.jpg'], :location => 'Punchbowl', :phone => 87644966, :specialities => 'Beef Wagyu', :opening_hours => '9:00:00', :closing_hours => '21:00:00')
 
 
 Review.destroy_all
@@ -120,7 +120,7 @@ l24 = Like.create
 # Association ------------
 
 #cuisines and restaurants
-c1.restaurants << r1 << r2 << r3
+c1.restaurants << r1 << r2 << r3 << r21
 c2.restaurants << r4 << r5 << r6
 c3.restaurants << r7 << r8 << r9
 c4.restaurants << r10 << r11 << r12 << r13
